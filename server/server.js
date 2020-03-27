b2ed9813fd4dcb7897330e996b5e71fa0c132c27
Ujjawal Jain <jainujjawal1999@gmail.com>
2020-03-27 15:29:51 +0530

    All routes set for TodoApp

--- server/server.js
+++ server/server.js
@@ -3,47 +3,8 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 const express = require('express');
 const bodyParser = require('body-parser');
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
+const {ObjectID} = require('mongodb');
 
-// var Todo = mongoose.model('Todo',{
-// 	text:{
-// 		type:String,
-// 		required : true,
-// 		minlength:1,
-// 		trim:true
-// 	},
-// 	completed:{
-// 		type:Boolean,
-// 		default:false
-// 	},
-// 	completedAt:{
-// 		type:Number,
-// 		default:null
-// 	}
-// });
-// var User = mongoose.model('User',{
-// 	email:{
-// 		type:String,
-// 		required:true,
-// 		minlength:1,
-// 		trim:true
-// 	}
-// });
-
-// var newTodo = new Todo({
-// 	text:'Cook Dinner',
-// });
-// newTodo.save().then((doc)=>console.log(doc)).catch((err)=>console.log(err));
-
-// var newUser = new User({
-// 	email:'jainujjawal1999@gmail.com'
-// });
-// newUser.save().then((res)=>{
-// 	console.log(res);
-// }).catch((err)=>{
-// 	console.log(err);
-// });
 var app = express();
 
 app.use(bodyParser.json());
@@ -66,6 +27,21 @@ app.get('/todos',(req,res)=>{
 	}).catch((err)=>res.status(400).send(err));
 });
 
+app.get('/todos/:id',(req,res)=>{
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+	Todo.findById(id).then((todo)=>{
+		if(!todo.length)
+			res.status(200).send(todo);
+		else
+			res.status(404).send();
+	}).catch((err)=>{
+		res.status(400).send('Not found');
+	});
+});
+
 app.post('/users',(req,res)=>{
 	var user = new User({
 		email:req.body.email
@@ -78,8 +54,30 @@ app.post('/users',(req,res)=>{
 		res.send(err);
 	});
 });
+
+app.get('/users',(req,res)=>{
+	User.find().then((users)=>{
+		res.status(200).send({users});
+	}).catch((err)=>res.status(404).send(err));
+});
+
+app.get('/users/:id',(req,res)=>{
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+	User.findById(id).then((user)=>{
+		if(!user.length)
+			res.status(200).send(user);
+		else
+			res.status(404).send('cshiahcs');
+	}).catch((err)=>{
+		res.status(400).send('csiahihwqeqf');
+	});
+});
+
 app.listen(3000,()=>{
 	console.log('Server up and running on port 3000');
 });
-
+ 
 module.exports = {app};
