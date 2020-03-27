bbee62df2bf489a619672caf5532326523a80555
Ujjawal Jain <jainujjawal1999@gmail.com>
2020-03-27 16:41:45 +0530

    Added tests for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,14 @@
 const expect =require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 var todos =[{
+	_id : new ObjectID(),
 	text:"First Todo"
 },{
+	_id : new ObjectID(),
 	text:"Second Todo"
 }];
 
@@ -61,6 +63,31 @@ describe('GET /todos',()=>{
 		.expect((res)=>{
 			expect(res.body.todos.length).toBe(2);
 		})
+		.end(done);
+	});
+});
+
+describe('GET /todos/:id',()=>{
+	it('should return todo with the id',(done)=>{
+		request(app)
+		.get(`/todos/${todos[0]._id.toHexString()}`)
+		.expect(200)
+		.expect((res)=>{
+			expect(res.body.text).toBe(todos[0].text);
+		})
+		.end(done);
+	});
+	it('should return a 400 if id not found',(done)=>{
+		var hexid = new ObjectID().toHexString();
+		request(app)
+		.get(`/todos/${hexid}`)
+		.expect(400)
+		.end(done);
+	});
+	it('should return a 404 for non-object ids',(done)=>{
+		request(app)
+		.get('/todos/123abs')
+		.expect(404)
 		.end(done);
 	});
 });
