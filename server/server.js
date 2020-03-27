9d6ce4ad669aaad370a6e1078e0c3836756e6775
Ujjawal Jain <jainujjawal1999@gmail.com>
2020-03-27 20:00:42 +0530

    Added patch route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 const express = require('express');
+const _ = require('lodash');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 const port = process.env.PORT || 3000;
@@ -54,6 +55,29 @@ app.delete('/todos/:id',(req,res)=>{
 			res.send({todo});
 	}).catch((err)=>{
 		res.send(400);
+	});
+});
+
+app.patch('/todos/:id',(req,res)=>{
+	var id = req.params.id;
+	var body = _.pick(req.body,["text","completed"]);
+	if(!ObjectID.isValid(id))
+		return res.status(404).send();
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completed = true;
+		body.completedAt = new Date().getTime();
+	}
+	else{
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findOneAndUpdate({_id:id},{$set:body},{new : true}).then((todo)=>{
+		if(!todo)
+			return res.status(404).send();
+		res.status(200).send(todo);
+	}).catch((err)=>{
+		res.status(400).send();
 	});
 });
 
